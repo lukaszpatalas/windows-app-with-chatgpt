baed07e68c7925110e83b063130d0d2a0116a914
Lukasz Patalas <lukasz.patalas86@gmail.com>
2023-03-12 20:04:35 +0000

    Update project files for .NET platform and C#
    The previous versions did not work, but after tweaking the code, the GUI
    window is finally appearing with an interactable button. This program
    outputs a single text expressing my everlasting love to my girlfriend,
    but it will serve as the backbone for more functionalities. The other
    buttons will connect to a chatGPT model and provide output.
    To be continued.

---
+++ Form1.cs
@@ -0,0 +1,33 @@
+namespace window_form;
+
+public partial class Form1 : Form
+{
+    private System.Windows.Forms.Button button1 = new System.Windows.Forms.Button();
+
+    public Form1()
+    {
+        InitializeComponent();
+        this.CenterToScreen(); // Center the form on the screen
+        AddButton(); // Call the AddButton method
+    }
+
+    private void AddButton()
+    {
+        button1.Text = "Click me!";
+        button1.Size = new System.Drawing.Size(100, 50);
+        button1.Location = new System.Drawing.Point(
+            (this.ClientSize.Width - button1.Width) / 2,
+            (this.ClientSize.Height - button1.Height) / 2
+        );
+        this.Controls.Add(button1);
+    }
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+        Label label = new Label();
+        label.Text = "Kocham Ilonke i inne czokodylki rubadylki";
+        label.AutoSize = true;
+        label.Location = new Point(button1.Location.X, button1.Location.Y + button1.Height + 10);
+        this.Controls.Add(label);
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,16 +1,16 @@
-using System;
-using System.Windows.Forms;
+namespace window_form;
 
-namespace WindowsApp
+static class Program
 {
-    static class Program
+    /// <summary>
+    ///  The main entry point for the application.
+    /// </summary>
+    [STAThread]
+    static void Main()
     {
-        [STAThread]
-        static void Main()
-        {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new GUI());
-        }
-    }
+        // To customize application configuration such as set high DPI settings or default font,
+        // see https://aka.ms/applicationconfiguration.
+        ApplicationConfiguration.Initialize();
+        Application.Run(new Form1());
+    }    
 }
